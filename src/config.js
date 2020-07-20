35ce162f8c3d82c0f02486fe09307af792fabb75
rick <rhennessey3@gmail.com>
2020-07-20 13:41:00 -0400

    added things

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
+    API_TOKEN: process.env.API_TOKEN || 'a38dc621-4bf4-4910-8f72-f2b831f74aad',
     DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/bookmarks',
 }
