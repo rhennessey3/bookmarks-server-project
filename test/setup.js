589ca7361e87ac5899d4aaf36eb577dfb1cfae84
rick <rhennessey3@gmail.com>
2020-07-21 18:14:54 -0400

    finished bookmarks thing

--- test/setup.js
+++ test/setup.js
@@ -1,8 +1,10 @@
+// process.env.TZ = 'UTC'
 process.env.NODE_ENV = 'test'
 process.env.API_TOKEN = 'test-auth-token'
 
 const { expect } = require('chai')
 const supertest = require('supertest')
+
 
 global.expect = expect
 global.supertest = supertest
